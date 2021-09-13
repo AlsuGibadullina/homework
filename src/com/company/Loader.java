d2c7a2955381d06d21d8d7d1c30a99a9bc352c8b
etoal <alsuwinx@mail.ru>
2021-09-13 23:21:39 +0300

    2k-174

--- src/com/company/Loader.java
+++ src/com/company/Loader.java
@@ -1,22 +1,10 @@
 package com.company;
 
-import com.company.Exceptions.NumberOfArgumentsException;
-import com.company.Exceptions.OperatorException;
-
 public class Loader {
     public static void main(String[] args) {
-        try {
-            Parser.checkNumberOfArguments(args.length);
-        } catch (NumberOfArgumentsException e) {
-            e.printStackTrace();
-        }
+        Parser.checkNumberOfArguments(args.length);
         var val1 = Parser.parseIntOrError(args[0]);
-        String operator = null;
-        try {
-            operator = Parser.parseOperatorOrError(args[1]);
-        } catch (OperatorException e) {
-            e.printStackTrace();
-        }
+        String operator = Parser.parseOperatorOrError(args[1]);
         var val2 = Parser.parseIntOrError(args[2]);
         System.out.println(Calculator.calculate(val1, operator, val2));
     }
